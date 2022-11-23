9d8fcf7e82dd002a32e4922157d05757a3f24ce8
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-11-23 20:01:07 +0100

    update application_vm.sh

--- application_vm.sh
+++ application_vm.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-sudo hostname EC2
+sudo hostname app-vm
 sudo apt update
 sudo apt upgrade -y
 sudo apt-get install git
 sudo apt-get install maven -y
 
-git clone git@github.com:hacizeynal/Deploying-Spring-PetClinic-Sample-Application-localy-using-Vagrant.git
+export DB_HOST=172.31.18.160
+export DB_PORT=3306
+export DB_NAME=petclinic
+export DB_USER=petclinic
+export DB_PASS=petclinic
+
+git clone https://github.com/hacizeynal/Deploying-Spring-PetClinic-Sample-Application-localy-using-Vagrant.git
 cd Deploying-Spring-PetClinic-Sample-Application-localy-using-Vagrant/spring-petclinic
 ./mvnw package
 java -jar target/*.jar
