4eafd8ef4c276d98bf175802da8cf893257ac9c0
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-11-27 00:00:45 +0100

    update application script

--- application_vm.sh
+++ application_vm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# UPDATE SYSTEM PACKAGES
 sudo apt update
 sudo apt upgrade -y
 sudo apt-get install git
@@ -8,15 +8,17 @@ sudo apt install net-tools
 sudo hostname app-vm
 
 
-
-export DB_HOST=172.31.18.239 >> ~/.bashrc # ip address for MySQL server
+# SET ENV VARIABLE FOR DB CONNECTION
+export DB_HOST=172.31.18.123 >> ~/.bashrc # ip address for MySQL server
 export DB_PORT=3306 >> ~/.bashrc # default port MySQL server
 export DB_NAME=petclinic >> ~/.bashrc # database name for MySQL server
 export DB_USER=petclinic >> ~/.bashrc # username for MySQL server
 export DB_PASS=petclinic >> ~/.bashrc # password for MySQL server
 source ~/.bashrc
 
+# GIT CLONE CODE ,BUILD AND RUN 
+
 git clone https://github.com/spring-projects/spring-petclinic.git
 cd spring-petclinic
-# ./mvnw package
-# java -jar target/*.jar &
+./mvnw package
+java -jar target/*.jar &
