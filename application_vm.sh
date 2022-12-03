c2414bcf9c429e28d46adbecd728536437c5a22f
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-12-03 15:11:49 +0100

    final version of app script

--- application_vm.sh
+++ application_vm.sh
@@ -7,7 +7,9 @@ sudo apt install net-tools
 git clone https://github.com/spring-projects/spring-petclinic.git
 cd spring-petclinic
 sudo sed -i "s/localhost/192.168.56.100:3306/g" src/main/resources/application-mysql.properties
+sudo sed -i "s/database=h2/database=mysql/g" src/main/resources/application.properties
+sudo bash -c 'echo "spring.profiles.active=mysql" >> src/main/resources/application.properties'
 echo "Package the application!"
-./mvnw package
+./mvnw package -Dcheckstyle.skip
 echo "Starting application!"
 java -jar target/*.jar &
