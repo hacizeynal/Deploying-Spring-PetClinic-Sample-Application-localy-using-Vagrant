9c091fa083ea6aa54f5736dbe6548cf9ec68b71d
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-12-03 15:12:03 +0100

    final version of db script

--- db_provision.sh
+++ db_provision.sh
@@ -1,6 +1,22 @@
-apt-get update
-apt-get install mysql-server -y
+#!/bin/bash
+sudo apt update -y
+sudo apt list --upgradable # get a list of upgrades
+sudo apt upgrade -y
+sudo apt-get install debconf-utils -y
+sudo apt-get install mysql-server -y
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
 sudo sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo service mysql restart
+
+echo "Setting ENV variable for DB password"
+echo "export DATABASE_PASS=1234" >> ~/.profile
+source ~/.bashrc
+
+echo "Configuring MySQL and importing user.sql"
 cd zhajili
-sudo mysql < user.sql
+sudo mysql -u root -p"$DATABASE_PASS" < user.sql 
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+
+echo "Restarting MySQL"
+sudo service mysql stop
+sudo service mysql start
